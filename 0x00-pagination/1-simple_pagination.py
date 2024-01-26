2e527504e569bcf78cb79938dcbc4b4551656921
2Kelvin <rocketmancodes02@gmail.com>
2024-01-26 18:57:28 +0300

    use try except block to fix error#7

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -24,8 +24,8 @@ class Server:
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
         '''returns a page's data'''
-        assert type(page) is int & page > 0
-        assert type(page_size) is int & page_size > 0
+        assert type(page) is int and page > 0
+        assert type(page_size) is int and page_size > 0
         pageWithBabyNames = self.dataset()
         try:
             (startIdx, endIdx) = index_range(page, page_size)
