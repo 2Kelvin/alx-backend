f622f30c57821ed84b2c133d37159491bad68df3
2Kelvin <rocketmancodes02@gmail.com>
2024-01-26 18:50:33 +0300

    use try except block to fix error

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -26,11 +26,12 @@ class Server:
         '''returns a page's data'''
         assert isinstance(page, int) & page > 0
         assert isinstance(page_size, int) & page_size > 0
-        (startIdx, endIdx) = index_range(page, page_size)
-        pageBabyNamesData = self.dataset()
-        if startIdx > len(pageBabyNamesData) | endIdx > len(pageBabyNamesData):
+        pageWithBabyNames = self.dataset()
+        try:
+            (startIdx, endIdx) = index_range(page, page_size)
+            return pageWithBabyNames[startIdx:endIdx]
+        except:
             return []
-        return pageBabyNamesData[startIdx:endIdx]
 
 
 def index_range(page: int, page_size: int) -> tuple:
