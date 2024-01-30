2df593cf7d42f8bcd515576914335d0a44e74309
2Kelvin <rocketmancodes02@gmail.com>
2024-01-30 22:38:50 +0300

    implement a lifo caching system

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''LIFO caching'''
+from collections import OrderedDict
+
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LIFOCache(BaseCaching):
+    '''LIFO caching'''
+
+    def __init__(self):
+        '''constructor'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''add cache data'''
+        if item is not None and key is not None:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    # getting the key to the last item in cache dict
+                    lastCacheKey = list(self.cache_data.keys())[-1]
+                    # discarding the last item
+                    del self.cache_data[lastCacheKey]
+                    print(f'DISCARD: {lastCacheKey}\n')
+
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        '''retrieve cache data'''
+        if key is None or not key:
+            return None
+        return self.cache_data.get(key)
