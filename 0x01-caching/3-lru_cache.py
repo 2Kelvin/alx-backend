e97bb2c44008293f89b0bd6003745621945962a4
2Kelvin <rocketmancodes02@gmail.com>
2024-01-30 23:02:04 +0300

    lru caching

---
+++ 0x01-caching/3-lru_cache.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+'''LRU Caching'''
+from collections import OrderedDict
+BaseCaching = __import__('base_caching').BaseCaching
+
+
+class LRUCache(BaseCaching):
+    '''Least Recently used caching policy'''
+
+    def __init__(self):
+        '''constructor'''
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        '''add cache data'''
+        if item is not None and key is not None:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > BaseCaching.MAX_ITEMS:
+                    leastUsedKey = list(self.cache_data.keys())[-1]
+                    del self.cache_data[leastUsedKey]
+                    print(f'DISCARD: {leastUsedKey}')
+                self.cache_data[key] = item
+                self.cache_data.move_to_end(key, last=False)
+            else:
+                self.cache_data[key] = item
+
+    def get(self, key):
+        '''retrieve cache data'''
+        if key is not None and key in self.cache_data:
+            self.cache_data.move_to_end(key, last=False)
+        return self.cache_data.get(key)
