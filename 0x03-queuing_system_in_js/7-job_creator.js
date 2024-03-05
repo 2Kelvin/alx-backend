d0c382b045b1c0fbfbcf8ba8a7a4e4ba0c010cac
2Kelvin <rocketmancodes02@gmail.com>
2024-03-05 11:53:15 +0300

    add tasks 8 & 9

---
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -0,0 +1,66 @@
+import kue, { createQueue } from 'kue';
+
+const jobs = [
+  {
+    phoneNumber: '4153518780',
+    message: 'This is the code 1234 to verify your account'
+  },
+  {
+    phoneNumber: '4153518781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153518743',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153538781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153118782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4153718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4159518782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4158718781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4153818782',
+    message: 'This is the code 4321 to verify your account'
+  },
+  {
+    phoneNumber: '4154318781',
+    message: 'This is the code 4562 to verify your account'
+  },
+  {
+    phoneNumber: '4151218782',
+    message: 'This is the code 4321 to verify your account'
+  }
+];
+
+const queue = createQueue({name: 'push_notification_code_2'});
+
+for (const job of jobs) {
+  const kueJob = queue.create('push_notification_code_2', job);
+
+  kueJob
+    .on('enqueue', () => console.log(`Notification job created: ${kueJob.id}`))
+    .on('complete', () => console.log(`Notification job ${kueJob.id} completed`))
+    .on('failed', (err) => console.log(`Notification job ${kueJob.id} failed: ${err}`))
+    .on('progress', (progress, data) => {
+        console.log(`Notification job ${kueJob.id} ${progress}% complete`)
+    });
+
+  kueJob.save((err) => {
+    if (err) console.log('Notification job failed');
+  });
+}
